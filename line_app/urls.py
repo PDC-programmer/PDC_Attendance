4b47034410b80b15367eccdaf9793a1039118cfd
PDC-programmer <it.helpdesk@plusdentalclinic.com>
2025-01-03 18:41:05 +0700

    add: employee register

--- line_app/urls.py
+++ line_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('callback/', views.callback, name='callback'),
+    path('register-employee/', views.register_employee, name='register_employee'),
 ]
