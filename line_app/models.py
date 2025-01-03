758168cc98e1e33ddc211b3637df5e7e82a722ef
PDC-programmer <it.helpdesk@plusdentalclinic.com>
2025-01-03 12:27:31 +0700

    reset: database

--- line_app/models.py
+++ line_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     # get from line
-    line_id = models.CharField(max_length=100, primary_key=True, verbose_name="LineID")
+    id = models.AutoField(primary_key=True)
+    line_id = models.CharField(max_length=100, verbose_name="LineID")
     line_name = models.CharField(max_length=100, verbose_name="LineName")
     line_picture_url = models.URLField(verbose_name="LinePictureURL")
     line_status_message = models.CharField(max_length=100, blank=True, null=True, verbose_name="LineStatus")
