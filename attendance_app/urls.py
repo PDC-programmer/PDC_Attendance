040dd6f5836b6d8f2db1a00b88b226953780d6ab
PDC-programmer <it.helpdesk@plusdentalclinic.com>
2025-01-08 14:43:11 +0700

    add: leave balance table and staff info on leave request page and add leavebalance line alert

--- attendance_app/urls.py
+++ attendance_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import leave_request_view, get_leave_types
+from .views import leave_request_view, get_leave_types, get_leave_balances, get_staff
 
 urlpatterns = [
     path('leave-request/', leave_request_view, name='leave_request'),
     path('get-leave-types/', get_leave_types, name='get_leave_types'),
+    path('get-leave-balances/', get_leave_balances, name='get_leave_balances'),
+    path('get-staff/', get_staff, name='get_staff'),
 ]
