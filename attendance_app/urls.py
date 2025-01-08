3770f3d16fb7a0a4513cf7310b6368ed4038a128
PDC-programmer <it.helpdesk@plusdentalclinic.com>
2025-01-08 15:09:20 +0700

    fix: add <str:user_id> on urls.py

--- attendance_app/urls.py
+++ attendance_app/urls.py
@@ -4,6 +4,6 @@ from .views import leave_request_view, get_leave_types, get_leave_balances, get_
 urlpatterns = [
     path('leave-request/', leave_request_view, name='leave_request'),
     path('get-leave-types/', get_leave_types, name='get_leave_types'),
-    path('get-leave-balances/', get_leave_balances, name='get_leave_balances'),
-    path('get-staff/', get_staff, name='get_staff'),
+    path('get-leave-balances/<str:user_id>/', get_leave_balances, name='get_leave_balances'),
+    path('get-staff/<str:user_id>/', get_staff, name='get_staff'),
 ]
