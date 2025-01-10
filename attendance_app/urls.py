7c214ce1be81fcf2c4b93553ede400f2c608454c
PDC-programmer <it.helpdesk@plusdentalclinic.com>
2025-01-10 18:54:09 +0700

    edit: use redirectUri to leaverequest

--- attendance_app/urls.py
+++ attendance_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import leave_request_view, get_leave_types, get_leave_balances, get_staff, get_leave_attendances
+from .views import (
+    leave_request_view,
+    get_leave_types,
+    get_leave_balances,
+    get_staff,
+    get_leave_attendances,
+    leave_request_detail,
+)
 
 urlpatterns = [
     path('leave-request/', leave_request_view, name='leave_request'),
@@ -7,4 +14,5 @@ urlpatterns = [
     path('get-leave-balances/<str:user_id>/', get_leave_balances, name='get_leave_balances'),
     path('get-staff/<str:user_id>/', get_staff, name='get_staff'),
     path('get-leave-attendances/<str:user_id>/', get_leave_attendances, name='get_leave_attendances'),
+    path("leave-request/<int:leave_id>/", leave_request_detail, name="leave_request_detail"),
 ]
