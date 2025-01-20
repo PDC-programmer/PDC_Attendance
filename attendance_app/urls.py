7934e477132320dba3ef36949d8d5c3944706df2
PDC-programmer <it.helpdesk@plusdentalclinic.com>
2025-01-20 15:14:20 +0700

    add: leave request list page

--- attendance_app/urls.py
+++ attendance_app/urls.py
@@ -1,20 +1,16 @@
 from django.urls import path
-from .views import (
-    leave_request_view,
-    get_leave_types,
-    get_leave_balances,
-    get_staff,
-    get_leave_attendances,
-    leave_request_detail,
-    leave_request_view_auth
-)
+from . import views
 
 urlpatterns = [
-    path('leave-request/', leave_request_view, name='leave_request'),
-    path('leave-request-auth/', leave_request_view_auth, name='leave_request_auth'),
-    path('get-leave-types/', get_leave_types, name='get_leave_types'),
-    path('get-leave-balances/', get_leave_balances, name='get_leave_balances'),
-    path('get-staff/', get_staff, name='get_staff'),
-    path('get-leave-attendances/', get_leave_attendances, name='get_leave_attendances'),
-    path("leave-request-detail/<int:leave_id>/", leave_request_detail, name="leave_request_detail"),
+    path('leave-request/', views.leave_request_view, name='leave_request'),
+    path('leave-request-auth/', views.leave_request_view_auth, name='leave_request_auth'),
+    path('get-leave-types/', views.get_leave_types, name='get_leave_types'),
+    path('get-leave-balances/', views.get_leave_balances, name='get_leave_balances'),
+    path('get-staff/', views.get_staff, name='get_staff'),
+    path('get-leave-attendances/', views.get_leave_attendances, name='get_leave_attendances'),
+    path("leave-request-detail/<int:leave_id>/", views.leave_request_detail, name="leave_request_detail"),
+
+    # New routes for leave requests overview and batch actions
+    path('leave-requests_list/', views.leave_requests_view, name='leave_requests_list'),
+    path('batch-action/', views.batch_action, name='batch_action'),
 ]
