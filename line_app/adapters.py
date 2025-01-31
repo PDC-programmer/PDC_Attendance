70dcdfbc1dc412135566cd727a8978737240130c
PDC-programmer <it.helpdesk@plusdentalclinic.com>
2025-01-31 17:37:42 +0700

    fix: remove first_name and last_name from adapters.py (MyCustomAccountAdapter)

--- line_app/adapters.py
+++ line_app/adapters.py
@@ -5,38 +5,31 @@ class MyCustomAccountAdapter(DefaultAccountAdapter):
 
     def save_user(self, request, user, form, commit=True):
         """
-        บันทึก User โดยไม่บันทึกค่า first_name และ last_name หาก Sign Up มาจาก Line
+        Saves a new `User` instance using information provided in the
+        signup form.
         """
-        # ตรวจสอบว่า Sign Up มาจาก Line หรือไม่
-        is_line_signup = request.session.get('socialaccount_provider') == 'line'
+        from allauth.account.utils import user_email, user_field, user_username
 
-        try:
-            # รับข้อมูลจากฟอร์ม
-            data = form.cleaned_data
-            email = data.get("email")
-            username = data.get("username")
-            user.email = email
-            user.username = username
-
-            # ถ้าไม่ใช่ Line ให้บันทึก first_name และ last_name
-            if not is_line_signup:
-                first_name = data.get("first_name")
-                last_name = data.get("last_name")
-                user.first_name = first_name or ''
-                user.last_name = last_name or ''
-
-            # ตั้งรหัสผ่าน
-            if "password1" in data:
-                user.set_password(data["password1"])
-            elif "password" in data:
-                user.set_password(data["password"])
-            else:
-                user.set_unusable_password()
-
-            if commit:
-                user.save()
-
-        except Exception as e:
-            print(f"Error saving user: {e}")
-
+        data = form.cleaned_data
+        first_name = "ชื่อ"
+        last_name = "นามสกุล"
+        email = data.get("email")
+        username = data.get("username")
+        user_email(user, email)
+        user_username(user, username)
+        if first_name:
+            user_field(user, "first_name", first_name)
+        if last_name:
+            user_field(user, "last_name", last_name)
+        if "password1" in data:
+            user.set_password(data["password1"])
+        elif "password" in data:
+            user.set_password(data["password"])
+        else:
+            user.set_unusable_password()
+        self.populate_username(request, user)
+        if commit:
+            # Ability not to commit makes it easier to derive from
+            # this adapter by adding
+            user.save()
         return user
