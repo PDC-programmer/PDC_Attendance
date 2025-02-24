59fb524d908875f805b7101b2e1899a156537a60
chatmongkol <it.helpdesk@plusdentalclinic.com>
2025-02-24 13:20:07 +0700

    test: add debug

--- ohochat_app/views.py
+++ ohochat_app/views.py
@@ -13,6 +13,7 @@ def oho_chat_webhook(request):
             receiver = data.get('receiver', 'Unknown')
             message = data.get('message', '')
             message_type = data.get('type', 'received')  # ค่าจะเป็น 'sent' หรือ 'received'
+            print(data)
 
             # บันทึกข้อความลง Database
             ChatMessage.objects.create(
