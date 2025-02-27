23a80984dfa40ae8d0fe3d8e66971b605d17a896
chatmongkol <it.helpdesk@plusdentalclinic.com>
2025-02-27 17:44:12 +0700

    edit: use bulk instead on shift_schedule_update

--- ohochat_app/models.py
+++ ohochat_app/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 from django.utils.timezone import now
 
-
-class ChatMessage(models.Model):
-    sender = models.CharField(max_length=255)
-    receiver = models.CharField(max_length=255)
-    message = models.TextField()
-    timestamp = models.DateTimeField(default=now)
-    message_type = models.CharField(max_length=50, choices=[('sent', 'Sent'), ('received', 'Received')])
-
-    def __str__(self):
-        return f"{self.sender} -> {self.receiver}: {self.message[:50]}"
