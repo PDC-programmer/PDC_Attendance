e3c105dcfe800c8d42483fbb8c06648acd8b6788
chatmongkol <it.helpdesk@plusdentalclinic.com>
2025-03-06 15:34:27 +0700

    add: approval_list_request_user

--- approval_app/urls.py
+++ approval_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import approval_list, approve_request, bulk_approve_requests
+from .views import *
 
 urlpatterns = [
     path("approval-list/", approval_list, name="approval_list"),
+    path("approval-list-request-user/", approval_list_request_user, name="approval_list_request_user"),
     path("approve/<int:approval_id>/<str:action>/", approve_request, name="approve_request"),
     path("bulk-update/", bulk_approve_requests, name="bulk_approve_requests"),
+    path("bulk-cancel/", bulk_cancel_requests, name="bulk_cancel_requests"),
 ]
